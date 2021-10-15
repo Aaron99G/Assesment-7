4ae17ad0b40e7e904ccd83a0d05467605193ee23
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-10-15 12:23:41 -0500

    third Commit

--- step3.js
+++ step3.js
@@ -38,3 +38,19 @@ console.log(hasUniqueChars('Moonday'))
 console.log(hasUniqueChars('Hello'))
 
 // PANGRAM SENTANCE
+function isPangram(str) {
+    sentance = str.toLowerCase();
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    for (let i = 0; i < alphabet.length; i++) {
+        let found = sentance.indexOf(alphabet[i])
+        if (found == -1) {
+            return false
+        }
+
+    }
+    return true
+}
+
+
+isPangram("The quick brown fox jumps over the lazy dog!")
