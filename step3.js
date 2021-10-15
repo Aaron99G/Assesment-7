13b41ab3cb9d06ecfc1862bef0921c00096d1f07
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-10-15 11:44:00 -0500

    third Commit

--- step3.js
+++ step3.js
@@ -1,10 +1,11 @@
+// SUM ZERO
 function addtoZero(arr) {
     for (let i = 0; i < arr.length; i++) {
         let num = arr[i]
         console.log(`num: ${num}`)
         for (let index = 0; index < arr.length; index++) {
             console.log(`Arr[index]: ${arr[index]}`)
-            if (num + arr[index === 0]) {
+            if (num + arr[index] === 0) {
                 return true
             }
 
@@ -14,4 +15,26 @@ function addtoZero(arr) {
     return false
 }
 
+console.log(addtoZero([]))
+console.log(addtoZero([1, 2, 3]))
 console.log(addtoZero([1, 2, 3, -2]))
+
+// UNIQUE CHARACTERS
+function hasUniqueChars(str) {
+    let newStr = str.split('')
+    for (let i = 0; i < newStr.length; i++) {
+        let char = newStr[i]
+        console.log(`char: ${char}`)
+        console.log(`next char: ${str[i + 1]}`)
+        if (char === newStr[i + 1]) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(hasUniqueChars('Monday'))
+console.log(hasUniqueChars('Moonday'))
+console.log(hasUniqueChars('Hello'))
+
+// PANGRAM SENTANCE
